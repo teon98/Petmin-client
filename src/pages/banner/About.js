9ef6393b0e6fd42e56d924ce0e826e9a5313bc49
teon98 <k55416325@gmail.com>
2023-08-02 16:41:19 +0900

    server와 연결을 위한 proxy 설정 및 test

--- src/pages/banner/About.js
+++ src/pages/banner/About.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BackTitleHeader from "../../components/BackTitleHeader";
 import styles from "../../styles/banner/layout.module.css";
+import axios from "axios";
 
 const About = () => {
+  useEffect(() => {
+    axios
+      .post("/petInformationRead/7")
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
   return (
     <div className={styles.frame}>
       <BackTitleHeader title="펫민소개" />
-      펫민 소개 페이지
+      springboot와 연결 잘되는지 test 함
     </div>
   );
 };
--- src/pages/banner/Hospital.js
+++ src/pages/banner/Hospital.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BackTitleHeader from "../../components/BackTitleHeader";
 import styles from "../../styles/banner/layout.module.css";
 
@@ -6,7 +6,6 @@ const Hospital = () => {
   return (
     <div className={styles.frame}>
       <BackTitleHeader title="병원목록" />
-      병원목록페이지
     </div>
   );
 };
