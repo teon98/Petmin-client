e30c65017d503cd6db3e483a674c9357c1b2adc1
teon98 <k55416325@gmail.com>
2023-08-04 15:04:57 +0900

    펫시터 등록 api 연결

--- src/components/PetSitterProfile/BasicInfoForm.js
+++ src/components/PetSitterProfile/BasicInfoForm.js
@@ -1,34 +1,124 @@
-import React from "react";
+import React, { useCallback, useRef } from "react";
 import style from "../../styles/PetSitterProfile.module.css";
+import { useState } from "react";
+import axios from "axios";
 
 const BasicInfoForm = () => {
+  const [about, setAbout] = useState("");
+  const [place, setPlace] = useState("");
+  const [placeImg, setPlaceImg] = useState([]);
+
+  //이미지 배열 받기
+  const imagesRef = useRef();
+
+  const handleChange = useCallback((e) => {
+    //console.log(imagesRef.current.files);
+    const {
+      target: { name, value },
+    } = e;
+
+    if (name === "about") {
+      setAbout(value);
+    }
+    if (name === "place") {
+      setPlace(value);
+    }
+    if (name === "placeImg") {
+      var imageList = [];
+      for (var i = 0; i < imagesRef.current.files.length; i++) {
+        imageList.push(imagesRef.current.files[i]);
+      }
+      //console.log(imageList);
+      setPlaceImg(imageList);
+    }
+  }, []);
+
+  const handlePost = () => {
+    console.log(about);
+    console.log(place);
+    console.log(placeImg);
+
+    var formData = new FormData();
+    //태영: userID는 추후 로그인한 사용자로 변경
+    formData.append("userId", "sampleUser12345");
+
+    for (var i = 0; i < placeImg.length; i++) {
+      formData.append("sitterHouse", placeImg[i]);
+    }
+
+    formData.append("sitterHousetype", place);
+    formData.append("sitterMsg", about);
+
+    axios
+      .post("/sitter/insert", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        alert("자기소개가 수정되었습니다!");
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div>
       <div className={style.subtitle}>자기소개</div>
       {/* 자기소개 textarea */}
       <div id={style.frame}>
-        <textarea id={style.about} placeholder="펫시터 님을 소개해주세요!" />
+        <textarea
+          id={style.about}
+          placeholder="펫시터 님을 소개해주세요!"
+          onChange={handleChange}
+          name="about"
+        />
       </div>
       <div className={style.subtitle}>거주형태</div>
       {/* 거주형태 radio */}
       <div id={style.frame}>
-        <input id="option1" type="radio" name="place" value="마당있는집" />
+        <input
+          id="option1"
+          type="radio"
+          name="place"
+          onChange={handleChange}
+          value="마당있는집"
+        />
         <label htmlFor="option1">마당있는집</label>
 
-        <input id="option2" type="radio" name="place" value="아파트" />
+        <input
+          id="option2"
+          type="radio"
+          name="place"
+          onChange={handleChange}
+          value="아파트"
+        />
         <label htmlFor="option2">아파트</label>
 
-        <input id="option3" type="radio" name="place" value="단독주택" />
+        <input
+          id="option3"
+          type="radio"
+          name="place"
+          onChange={handleChange}
+          value="단독주택"
+        />
         <label htmlFor="option3">단독주택</label>
       </div>
       <div className={style.subtitle}>거주지 이미지</div>
       {/* 거주지 이미지 file input */}
       <div id={style.frame}>
-        <input type="file" multiple />
+        <input
+          name="placeImg"
+          type="file"
+          multiple
+          ref={imagesRef}
+          onChange={handleChange}
+        />
       </div>
 
       <div className={style.saveBT} id={style.frame}>
-        <input type="button" value="저장" />
+        <input type="button" value="저장" onClick={handlePost} />
       </div>
     </div>
   );
--- src/components/PetSitterProfile/ScheduleForm.js
+++ src/components/PetSitterProfile/ScheduleForm.js
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import style from "../../styles/PetSitterProfile.module.css";
 import { DayPicker } from "react-day-picker";
+import "../../styles/daypickerCustom.css";
+import { usePopper } from "react-popper";
+import { format, isValid, parse } from "date-fns";
+import FocusTrap from "focus-trap-react";
 
 const ScheduleForm = () => {
+  const [selected, setSelected] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [isPopperOpen, setIsPopperOpen] = useState(false);
+
+  const popperRef = useRef(null);
+  const buttonRef = useRef(null);
+  const [popperElement, setPopperElement] = useState(null);
+
+  const popper = usePopper(popperRef.current, popperElement, {
+    placement: "bottom-start",
+  });
+
+  const closePopper = () => {
+    setIsPopperOpen(false);
+    buttonRef?.current?.focus();
+  };
+
+  const handleInputChange = (e) => {
+    console.log(e);
+    setInputValue(e.target.value);
+    const date = parse(e.target.value, "y-MM-dd", new Date());
+    if (isValid(date)) {
+      setSelected(date);
+    } else {
+      setSelected(undefined);
+    }
+  };
+
+  const handleButtonClick = () => {
+    setIsPopperOpen(true);
+  };
+
+  const handleDaySelect = (date) => {
+    console.log(date);
+    setSelected(date);
+    if (date) {
+      setInputValue(format(date, "y-MM-dd"));
+      closePopper();
+    } else {
+      setInputValue("");
+    }
+  };
+
   return (
     <div>
       <div id={style.frame}>
@@ -10,9 +57,90 @@ const ScheduleForm = () => {
         <span style={{ color: "#17A1FA" }}>■예약완료</span>
       </div>
       <div id={style.frame}></div>
+      <div id={style.frame}>
+        <div ref={popperRef}>
+          <input
+            size={12}
+            type="text"
+            placeholder={format(new Date(), "y-MM-dd")}
+            value={inputValue}
+            onChange={handleInputChange}
+            ref={buttonRef}
+            onClick={handleButtonClick}
+            className={style.datepickInput}
+          />
+          {/* <button type="button">날짜 선택하기</button> */}
+        </div>
+        {isPopperOpen && (
+          <FocusTrap
+            active
+            focusTrapOptions={{
+              initialFocus: false,
+              allowOutsideClick: true,
+              clickOutsideDeactivates: true,
+              onDeactivate: closePopper,
+              fallbackFocus: buttonRef.current,
+            }}
+          >
+            <div
+              tabIndex={-1}
+              style={popper.styles.popper}
+              className="dialog-sheet"
+              {...popper.attributes.popper}
+              ref={setPopperElement}
+              role="dialog"
+              aria-label="DayPicker calendar"
+            >
+              <DayPicker
+                initialFocus={isPopperOpen}
+                mode="single"
+                defaultMonth={selected}
+                fromYear={2023}
+                selected={selected}
+                onSelect={handleDaySelect}
+                showOutsideDays
+              />
+            </div>
+          </FocusTrap>
+        )}
+      </div>
       <div id={style.frame} style={{ textAlign: "right" }}>
         <input id="allday" type="checkbox" />
         <label htmlFor="allday">종일 가능</label>
+      </div>
+      <div>
+        <div className={style.subtitle3}>오전</div>
+        <div className={style.timetable}>
+          <button className={style.timeBT}>6:00</button>
+          <button className={style.timeBT}>7:00</button>
+          <button className={style.timeBT}>8:00</button>
+          <button className={style.timeBT}>9:00</button>
+          <button className={style.timeBT}>10:00</button>
+          <button className={style.timeBT}>11:00</button>
+          <button className={style.timeBT}>12:00</button>
+        </div>
+        <div className={style.subtitle3}>오후</div>
+        <div className={style.timetable}>
+          <button className={style.timeBT}>13:00</button>
+          <button className={style.timeBT}>14:00</button>
+          <button className={style.timeBT}>15:00</button>
+          <button className={style.timeBT}>16:00</button>
+          <button className={style.timeBT}>17:00</button>
+          <button className={style.timeBT}>18:00</button>
+          <button className={style.timeBT}>19:00</button>
+          <button className={style.timeBT}>20:00</button>
+          <button className={style.timeBT}>21:00</button>
+          <button className={style.timeBT}>22:00</button>
+          <button className={style.timeBT}>23:00</button>
+          <button className={style.timeBT}>24:00</button>
+        </div>
+        <div
+          className={style.saveBT}
+          id={style.frame}
+          style={{ textAlign: "right" }}
+        >
+          <input type="button" value="저장" />
+        </div>
       </div>
     </div>
   );
