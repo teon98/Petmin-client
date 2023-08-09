f2392b9c2bedd71cda44ba42fecf4656ae1b01f1
e-7281998 <jejjcs43@naver.com>
2023-08-09 19:21:32 +0900

    feat : 사용자ID 정보 더미 -> recoil의 userId로 수정

--- src/pages/User/CardInfo.js
+++ src/pages/User/CardInfo.js
@@ -4,6 +4,8 @@ import BackTitleHeader from "../../components/BackTitleHeader";
 import PinkBtn from "../../components/User/PinkBtn";
 import styled from "styled-components";
 import axios from "axios";
+import { useRecoilState } from "recoil";
+import { idtextAtom, userCardAtom } from "../../atom/atoms";
 
 const CardForm = styled.div`
   .mb {
@@ -64,6 +66,8 @@ function CardInfo(props) {
   });
   const [userCard, setUserCard] = useState([]);
   const [msg, setMsg] = useState("");
+  const [userId] = useRecoilState(idtextAtom);
+  const [success, setSuccess] = useState(false);
 
   //페이지 로딩 시 유저 정보 가져오기
   useEffect(() => {
@@ -71,7 +75,7 @@ function CardInfo(props) {
       url: "/user",
       method: "get",
       params: {
-        userId: "admin",
+        userId: userId,
       },
     })
       .then((res) => {
@@ -154,7 +158,7 @@ function CardInfo(props) {
       url: "/user/cardRegister",
       method: "put",
       data: {
-        userId: "admin",
+        userId: userId,
         userCard: cardNumberStr,
         userCardpass: cardPass,
       },
@@ -163,6 +167,7 @@ function CardInfo(props) {
         setBtnState(false);
         setCardPass("");
         setMsg(() => "");
+        setSuccess(true);
       })
       .catch((err) => {
         console.log(err);
@@ -201,7 +206,9 @@ function CardInfo(props) {
         />
       </div>
       <PinkBtn
-        title={userCard.length === 0 ? btnTextValue[0] : btnTextValue[1]}
+        title={
+          userCard.length === 0 || !success ? btnTextValue[0] : btnTextValue[1]
+        }
         active={btnState}
         onClick={onClick}
       />
