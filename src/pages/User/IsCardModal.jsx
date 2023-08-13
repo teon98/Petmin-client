55330a8f690240dfc8d78d80e627dbd8e34aaa02
e-7281998 <jejjcs43@naver.com>
2023-08-14 08:20:37 +0900

    feat : 보험 시 카드 모달 style 수정

--- src/pages/User/IsCardModal.jsx
+++ src/pages/User/IsCardModal.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { idtextAtom, userCardNumber } from "../../atom/atoms";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { bottom } from "@popperjs/core";
 
 const ModalOverlay = styled.div`
   position: fixed;
@@ -19,12 +20,52 @@ const ModalOverlay = styled.div`
 `;
 
 const ModalContent = styled.div`
+  font-family: PreMedium;
+  width: 90%;
+  height: 220px;
+  box-sizing: border-box;
   background-color: #fff;
   padding: 20px;
+  padding-bottom: 10px;
   border-radius: 10px;
   display: flex;
   justify-content: center;
   align-items: center;
+  position: absolute;
+
+  p,
+  input {
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+`;
+
+const BoxBtn = styled.input`
+  width: 100%;
+  height: 39px;
+  border-radius: 5px;
+  background: #f66;
+  color: #ffff;
+  font-weight: 900;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: white;
+  transition: background-color 0.3s ease-in-out;
+
+  &:hover {
+    background-color: #ff9999;
+  }
+`;
+
+const CloseBtn = styled.input`
+  position: absolute;
+  top: 0;
+  right: 10px;
+  border: none;
+  background: transparent;
+  color: #f66;
+  border-radius: 5px;
 `;
 
 const IsCardModal = ({ onClose }) => {
@@ -41,6 +82,7 @@ const IsCardModal = ({ onClose }) => {
 
   //비밀번호 입력 후 확인
   const CheckPwd = () => {
+    if (pwd.length !== 4) return;
     axios({
       method: "get",
       url: "/user/checkCard",
@@ -54,7 +96,7 @@ const IsCardModal = ({ onClose }) => {
         setMsg("결제가 완료되었습니다.");
         setTimeout(() => {
           nav("/checkSitter");
-        }, 3000);
+        }, 2000);
       } else {
         //비밀번호 불일치
         setPwd("");
@@ -75,11 +117,18 @@ const IsCardModal = ({ onClose }) => {
     <ModalOverlay>
       <ModalContent>
         <p style={{ color: "#f66" }}>
-          <input type="button" value="X" onClick={onClose} />
+          <CloseBtn
+            style={{ fontSize: "20px" }}
+            type="button"
+            value="X"
+            onClick={onClose}
+          />
           {cardNumber[0] === "" ? (
             <div>
-              <p>등록된 카드가 없습니다.</p>
-              <input
+              <p style={{ marginBottom: "30px", color: "#333" }}>
+                등록된 카드가 없습니다.
+              </p>
+              <BoxBtn
                 type="button"
                 value="카드 등록하기"
                 onClick={RegisterCard}
@@ -87,10 +136,26 @@ const IsCardModal = ({ onClose }) => {
             </div>
           ) : (
             <div>
-              <p>카드 비밀번호를 입력해주세요.</p>
-              <input value={pwd} maxLength="4" onChange={changePwd} />
-              <input type="button" value="결제하기" onClick={CheckPwd} />
-              <p>{msg}</p>
+              <p style={{ color: "#333" }}>카드 비밀번호를 입력해주세요.</p>
+              <input
+                type="password"
+                style={{ width: "100%", textAlign: "center" }}
+                value={pwd}
+                maxLength="4"
+                onChange={changePwd}
+                placeholder="XXXX"
+              />
+              <BoxBtn type="button" value="결제하기" onClick={CheckPwd} />
+              <p
+                style={{
+                  height: "15px",
+                  textAlign: "center",
+                  fontSize: "12px",
+                }}
+              >
+                {" "}
+                {msg}
+              </p>
             </div>
           )}
         </p>
