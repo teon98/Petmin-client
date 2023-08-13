40c6245f639406321dada0cf06ef19bbfb5e1598
dltlaos11 <dltlaos14@naver.com>
2023-08-13 11:40:56 +0900

    feat: add review-2

--- src/components/FooterPS.js
+++ src/components/FooterPS.js
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
+
 import style from "../styles/PSView.module.css";
-import { useNavigate } from "react-router";
+import { useNavigate, useLocation } from "react-router";
+import { useRecoilState } from "recoil";
+import { idtextAtom, nametextAtom } from "../atom/atoms";
+import axios from "axios";
 
 const FooterPS = (props) => {
   const nav = useNavigate();
+  const location = useLocation();
+
+  const [startId, setStartId] = useRecoilState(idtextAtom);
+  const receiver =
+    location.pathname.split("/")[location.pathname.split("/").length - 1];
+
+  async function makeRoom(s, r) {
+    const url = "/chat/chatting";
+    axios
+      .get(url, {
+        headers: {
+          "Content-Type": `application/json`,
+        },
+        params: {
+          sender: s,
+          receiver: r,
+        },
+      })
+      .then((res) => {
+        console.log(res.data.chatroomId);
+        nav(`/room/${res.data.chatroomId}/${receiver}`);
+      })
+      .catch((ex) => {
+        console.log("requset fail : " + ex);
+      });
+  }
+
   console.log("profileName--------------");
   console.log(props.userId);
   return (
     <div className={style.buttonFooter}>
-      <button id={style.a}>문의하기</button>
+      <button id={style.a} onClick={() => makeRoom(startId, receiver)}>
+        문의하기
+      </button>
       <button
         id={style.b}
         onClick={() => {
