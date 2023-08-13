090c7d91487ceba2a6a39b10e21350c3b18e03a0
jiman-you <80306786+jiman-you@users.noreply.github.com>
2023-08-13 12:02:08 +0900

    병원 아이콘 , 골드 테스트 추가

--- src/components/FooterPS.js
+++ src/components/FooterPS.js
@@ -1,14 +1,34 @@
 import React from "react";
 import style from "../styles/PSView.module.css";
 import { useNavigate } from "react-router";
+import { idtextAtom } from "../atom/atoms";
+import { useRecoilState } from "recoil";
+import axios from "axios";
 
 const FooterPS = (props) => {
   const nav = useNavigate();
   console.log("profileName--------------");
   console.log(props.sitter);
+  const [userId] = useRecoilState(idtextAtom);
+
+  const dolbomchat = (sangdaeId) => {
+    axios({
+      url: "/chat/chatting",
+      params: { sender: userId, receiver: props.sitterId },
+      method: "get",
+    })
+      .then((res) => {
+        nav("/room/" + res.data.chatroomId + "/" + props.sitterId);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className={style.buttonFooter}>
-      <button id={style.a}>문의하기</button>
+      <button id={style.a} onClick={dolbomchat}>
+        문의하기
+      </button>
       <button
         id={style.b}
         onClick={() => {
